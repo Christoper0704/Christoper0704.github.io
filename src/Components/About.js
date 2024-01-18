8f8c3c80bba4d47076a4db178d857d7890293cba
Christoper0704 <c.luis0704@gmail.com>
2024-01-18 20:10:52 +0700

    Update About & Navbar

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,20 +1,28 @@
 import React from 'react';
-import '../Styles/About.css'
+import '../Styles/About.css';
+import selfie from '../Images/foto.png';
 
 const About = () => {
   return (
-    <div id='about'>
-      <div className='about-image'>
-        <div className='about-text'>
-          <div className>
-            <img src={require('../Images/selfie.jpg')} className='figure' alt='myselfphoto'></img>
-          </div>
-          <h1 >I'm Christoper Luis Alexander</h1>
-          <br></br>
-          <h3>App Developer interested such as web and Android.</h3>
-        </div>
+    <section id='about'>
+      <div className='about-content'>
+        <span className='about-intro'>I'm <span className='about-name'>Christoper Luis Alexander</span><br /> Application Developer</span>
+        <p className='about-para'>App Developer interested such as Web and Android.</p>
       </div>
-    </div>
+      <img className='about-image' src={selfie} alt='profile'/>
+    </section>
+    // <div id='about'>
+    //   <div className='about-image'>
+    //     <div className='about-text'>
+    //       <div className>
+    //         <img src={require('../Images/selfie.jpg')} className='figure' alt='myselfphoto'></img>
+    //       </div>
+    //       <h1 >I'm Christoper Luis Alexander</h1>
+    //       <br></br>
+    //       <h3>App Developer interested such as web and Android.</h3>
+    //     </div>
+    //   </div>
+    // </div>
   )
 }
 
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import navSticky from './navSticky';
+import { Link } from 'react-scroll';
 import '../Styles/Navbar.css'
 
 const Navbar = () => {
@@ -35,30 +35,39 @@ const Navbar = () => {
   }
 
   return (
-      <nav className={x.join(" ")}>
-        <div className='navbar-container'>
-          <div className='navbar-logo'>
-            <h3>PERSONAL WEBSITE</h3>
-          </div>
-          <div className='menu-icon' onClick={openMenu}>
-            <i className={showMenu ? 'fas fa-times' : 'fas fa-bars'} />
-          </div>
-            <ul className={showMenu ? 'navbar-menu active' : 'navbar-menu'}>
-              <li>
-                <a href="#about" className='navbar-link' onClick={closeMenu}>About Me</a>
-              </li>
-              <li>
-                <a href="#skill" className='navbar-link' onClick={closeMenu}>Skill</a>
-              </li>
-              <li>
-                <a href="#project" className='navbar-link' onClick={closeMenu}>Project</a>
-              </li>
-              <li>
-                <a href="#contact" className='navbar-link' onClick={closeMenu}>Contact Me</a>
-              </li>
-            </ul>
-        </div>
-      </nav>
+    <nav className='navbar'>
+      <div className='navMenu'>
+        <Link className='navMenuItem'>About</Link>
+        <Link className='navMenuItem'>Skill</Link>
+        <Link className='navMenuItem'>Project</Link>
+        <Link className='navMenuItem'>Contact</Link>
+      </div>
+      <div></div>
+    </nav>
+      // <nav className={x.join(" ")}>
+      //   <div className='navbar-container'>
+      //     <div className='navbar-logo'>
+      //       <h3>PERSONAL WEBSITE</h3>
+      //     </div>
+      //     <div className='menu-icon' onClick={openMenu}>
+      //       <i className={showMenu ? 'fas fa-times' : 'fas fa-bars'} />
+      //     </div>
+      //       <ul className={showMenu ? 'navbar-menu active' : 'navbar-menu'}>
+      //         <li>
+      //           <a href="#about" className='navbar-link' onClick={closeMenu}>About Me</a>
+      //         </li>
+      //         <li>
+      //           <a href="#skill" className='navbar-link' onClick={closeMenu}>Skill</a>
+      //         </li>
+      //         <li>
+      //           <a href="#project" className='navbar-link' onClick={closeMenu}>Project</a>
+      //         </li>
+      //         <li>
+      //           <a href="#contact" className='navbar-link' onClick={closeMenu}>Contact Me</a>
+      //         </li>
+      //       </ul>
+      //   </div>
+      // </nav>
   )
 }
 
