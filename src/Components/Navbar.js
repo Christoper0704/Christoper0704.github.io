9d1094a93ea3f2cdc0617e21c6ecc3ff87d20f39
Christoper0704 <c.luis0704@gmail.com>
2024-01-21 19:54:55 +0700

    Update Navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,35 +5,6 @@ import menuBar from '../Images/menu.png';
 
 const Navbar = () => {
   const [showMenu, setShowMenu] = useState(false);
-  const [scroll, setScroll] = useState(false);
-
-  const openMenu = () =>
-  {
-    setShowMenu(!showMenu);
-  }
-  
-  const closeMenu = () => setShowMenu(false);
-
-  const handleScroll = () => {
-    const offset = window.scrollY;
-    if(offset > 200)
-    {
-      setScroll(true);
-    }
-    else
-    {
-      setScroll(false);
-    }
-  }
-
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll)
-  })
-  
-  let x = ['navbar'];
-  if(scroll){
-    x.push('scroll');
-  }
 
   return (
     <nav className='navbar'>
@@ -53,30 +24,6 @@ const Navbar = () => {
         {/* <Link activeClass='active' to='contact' spy={true} smooth={true} offset={-50} duration={500} className='navMenuItem'>Contact</Link> */}
       </div>
     </nav>
-      // <nav className={x.join(" ")}>
-      //   <div className='navbar-container'>
-      //     <div className='navbar-logo'>
-      //       <h3>PERSONAL WEBSITE</h3>
-      //     </div>
-      //     <div className='menu-icon' onClick={openMenu}>
-      //       <i className={showMenu ? 'fas fa-times' : 'fas fa-bars'} />
-      //     </div>
-      //       <ul className={showMenu ? 'navbar-menu active' : 'navbar-menu'}>
-      //         <li>
-      //           <a href="#about" className='navbar-link' onClick={closeMenu}>About Me</a>
-      //         </li>
-      //         <li>
-      //           <a href="#skill" className='navbar-link' onClick={closeMenu}>Skill</a>
-      //         </li>
-      //         <li>
-      //           <a href="#project" className='navbar-link' onClick={closeMenu}>Project</a>
-      //         </li>
-      //         <li>
-      //           <a href="#contact" className='navbar-link' onClick={closeMenu}>Contact Me</a>
-      //         </li>
-      //       </ul>
-      //   </div>
-      // </nav>
   )
 }
 
